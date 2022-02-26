1fec39fbb27996b61c2af696f97513bf3e114a0a
Connor Markwell <markwell.connor@gmail.com>
2022-02-25 23:44:23 -0500

    Add ability to specify length and count of generated NLPs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 use rand::Rng;
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+
     let (adjectives, num_adjs) = parse_bytes(include_bytes!("resources/adjectives.txt"));
     let (nouns, num_nouns) = parse_bytes(include_bytes!("resources/nouns.txt"));
 
     let mut rng = rand::thread_rng();
-    let dice_adj: usize = rng.gen_range(0..num_adjs) as usize;
-    let dice_noun: usize = rng.gen_range(0..num_nouns) as usize;
 
-    print!("{} {}", adjectives[dice_adj], nouns[dice_noun]);
+    for _ in 0..config.count {
+        let mut nlp = String::new();
+        for _ in 0..config.length {
+            let dice_adj = rng.gen_range(0..num_adjs) as usize;
+            let dice_noun = rng.gen_range(0..num_nouns) as usize;
+            nlp.push_str(&format!("{} {} ", adjectives[dice_adj], nouns[dice_noun]));
+        }
+        println!("{}", nlp);
+    }
+}
+
+struct Config {
+    length: i32,
+    count: i32,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let length = args[1].clone().parse().unwrap();
+        let count = args[2].clone().parse().unwrap();
+        Config { length, count }
+    }
 }
 
 /// Parse newline-delineated strings from an array of bytes.
